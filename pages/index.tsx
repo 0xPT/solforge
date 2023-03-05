5aeabace84efa17b1bf84d2a9ec717e7117012e7
0xPT <paul@paulthomas.io>
2023-03-05 02:00:37 -0700

    chakra..

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import { ReactFlowProvider } from "reactflow";
 import { OutputProvider } from "../hooks/useOutput";
 
 export const Theme = extendTheme({
+  initialColorMode: "light",
+  //  If true, your app will change color mode based on the user's system preferences.
+  useSystemColorMode: false,
   fonts: {
     heading: "Inter",
     body: "Inter",
